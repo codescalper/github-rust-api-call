f34a443d818da6a79c380113059f2a959e70685f
codescalper <msfunbook@gmail.com>
2023-10-23 20:08:39 +0530

    `hard-coded value` -> `user-input`

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use reqwest::header::USER_AGENT;
 use reqwest::Error;
 use serde::Deserialize;
-
+use std::io;
 #[derive(Deserialize, Debug)]
 struct User {
     login: String,
@@ -10,11 +10,19 @@ struct User {
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let request_url = format!(
-        "https://api.github.com/repos/{owner}/{repo}/stargazers",
-        owner = "codescalper",
-        repo = "mern-todo"
-    );
+    let mut owner = String::new();
+    println!("Please enter the name of the user: ");
+    io::stdin()
+        .read_line(&mut owner)
+        .expect("Failed to read line");
+
+    let mut repo = String::new();
+    println!("Please enter the name of the user's repo: ");
+    io::stdin()
+        .read_line(&mut repo)
+        .expect("Failed to read line");
+
+    let request_url = format!("https://api.github.com/repos/{owner}/{repo}/stargazers",);
     println!("{:#?}", request_url);
     let client = reqwest::Client::new();
     let response = client
