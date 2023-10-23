bd70c3ed3d8f18d2e599bf38821615aaf8382c22
codescalper <msfunbook@gmail.com>
2023-10-23 19:48:33 +0530

    mad the project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::header::USER_AGENT;
+use reqwest::Error;
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct User {
+    login: String,
+    id: u32,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let request_url = format!(
+        "https://api.github.com/repos/{owner}/{repo}/stargazers",
+        owner = "codescalper",
+        repo = "mern-todo"
+    );
+    println!("{:#?}", request_url);
+    let client = reqwest::Client::new();
+    let response = client
+        .get(&request_url)
+        .header(USER_AGENT, "rust github-api call project")
+        .send()
+        .await?;
+
+    let users: Vec<User> = response.json().await?;
+    println!("{:?}", users);
+
+    Ok(())
 }
