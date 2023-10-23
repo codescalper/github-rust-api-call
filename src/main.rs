efc00f542e404ef1a8ad0f3e4056f67b8a6ada74
codescalper <msfunbook@gmail.com>
2023-10-23 22:28:20 +0530

    added comments for better understanding

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,17 @@ use reqwest::Error;
 use serde::Deserialize;
 use std::io;
 #[derive(Deserialize, Debug)]
+// * Since we are using json and rust don't understand json directly, Hence we are going to deserialize it to strcuts so that rust can understand
 struct User {
     login: String,
     id: u32,
 }
 
-#[tokio::main]
+#[tokio::main] //* Attribute helps to make function async
 async fn main() -> Result<(), Error> {
     let mut owner = String::new();
     println!("Please enter the name of the user: ");
-    io::stdin()
+    io::stdin() //* fn for user input
         .read_line(&mut owner)
         .expect("Failed to read line");
 
@@ -24,14 +25,14 @@ async fn main() -> Result<(), Error> {
 
     let request_url = format!("https://api.github.com/repos/{owner}/{repo}/stargazers",);
     println!("{:#?}", request_url);
-    let client = reqwest::Client::new();
+    let client = reqwest::Client::new(); // * Create a new instance of a reqwest HTTP client.
     let response = client
         .get(&request_url)
         .header(USER_AGENT, "rust github-api call project")
         .send()
         .await?;
 
-    let users: Vec<User> = response.json().await?;
+    let users: Vec<User> = response.json().await?; // * Since the repo can be starred by many user we are using vector to store the user
     println!("{:?}", users);
 
     Ok(())
